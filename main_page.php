884f691c70c65093a209922c8fa444835a01ae69
vasiliskiosses97 <44527381+vasiliskiosses97@users.noreply.github.com>
2021-01-03 19:28:17 +0200

    Create main_page.php

---
+++ main_page.php
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-center ">
+  <div class="btn-group-vertical btn-block col-md-4 ">
+    <a href="index.php?ctd=game" class="btn btn-danger btn-lg">Play</a> <br>
+    <a href="https://beatcasino.gr/tavli/plakwto/" target="_blank" class="btn btn-danger btn-lg">Plakoto Rules</a> <br>
+    <a href="index.php?ctd=my_account" class="btn btn-danger btn-lg">My Account</a> <br>
+    <a href="index.php?ctd=logout" class="btn btn-danger btn-lg">Logout</a> <br>
+</div>
+</div>
